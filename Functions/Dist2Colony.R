0a1a7095c93981e7c47d1624f77ced3ebf2f0c2e
abfleishman <abfleishman@gmail.com>
2018-05-02 11:32:17 -0700

    remove comments and add argosfilter to the distance

--- Functions/Dist2Colony.R
+++ Functions/Dist2Colony.R
@@ -11,13 +11,11 @@
 #'
 #' @export
 Dist2Colony<-function(tracks,ColonyLat,ColonyLong){
-  # Step 1: Two packages for mapping.####
 
-  # Step 4a: Calculate distance from each point to the San Jorge center point.
   Point2Colony<-vector(mode = "numeric",length = nrow(tracks))
   for(i in 1:length(tracks$Latitude)){ #this is a for loop
     # This is a function to calculate distance between two points from the argosfilter package
-    Point2Colony[i]<-distance(lat1 = ColonyLat,lon1 = ColonyLong ,lat2 = tracks$Latitude[i],lon2 = tracks$Longitude[i])
+    Point2Colony[i]<-argosfilter::distance(lat1 = ColonyLat,lon1 = ColonyLong ,lat2 = tracks$Latitude[i],lon2 = tracks$Longitude[i])
 
   }
   return(Point2Colony)
